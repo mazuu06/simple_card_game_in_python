b80bf565ae7d463763c9f92117527e0ed93a5289
RaNa MazHar Naseer <103133005+mazuu06@users.noreply.github.com>
2022-07-04 19:47:43 +0500

    Add files via upload

---
+++ card.py
@@ -0,0 +1,61 @@
+import random
+
+class Card(object):
+    def __init__(self, suit, val):
+        self.suit = suit
+        self.value = val
+        
+    def show(self):
+        print("{} of {}".format(self.value, self.suit))
+        
+        
+class Deck(object):
+    def __init__(self):
+        self.cards = []
+        self.build()
+        
+    def build(self):
+        for s in ["Spades", "Clubs", "Diamonds", "Hearts"]:
+            for m in range(1, 14):
+                self.cards.append(Card(s ,m))
+                
+    def show(self):
+        for c in self.cards:
+            c.show()
+            
+    def shuffle(self):
+        for i in range(len(self.cards)-1, 0, -1):
+            r = random.randint(0, i)
+            self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+            
+    def drawCard(self):
+        return self.cards.pop()
+            
+class player(object):
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+        
+    def draw(self, deck):
+        self.hand.append(deck.drawCard())
+        return self
+        
+    def showHand(self):
+        for card in self.hand:
+            card.show()
+            
+    def discord(self):
+        return self.hand.pop()
+    
+# card = Card("Clubs", 6)
+# card.show()
+
+
+deck = Deck()
+deck.shuffle()
+deck.show()
+
+
+# bob =  player("Bob")
+# bob.draw(deck).draw(deck)
+# bob.showHand()
